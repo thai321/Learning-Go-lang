66fbd246050600f0ddc07d38ff3d3187fc24f9c8
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-12-02 17:31:10 -0800

    Write deck to file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
@@ -24,5 +28,28 @@ func newDeck() deck {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - Log the error and return a call to newDeck()
+
+		// Option #2 - Log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 }
--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,11 @@
 package main
 
+// import "fmt"
+
 func main() {
 	cards := newDeck()
-
-	cards.print()
+	// fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
 
 func newCard() string {
